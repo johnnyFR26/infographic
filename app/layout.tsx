e413b7ceac2da04d107961c2e590d96f02790bff
johnnyFR26 <johnny.rabelo.cf@gmail.com>
2025-09-12 11:15:10 -0300

    feat: adiciona favicon no projeto

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,9 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'Security infographic',
   description: 'Infográfico interativo sobre protocolos de segurança web',
+  icons: {
+    icon: "/favicon.ico"
+  }
 }
 
 export default function RootLayout({
